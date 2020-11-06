ca2a4076761393c3024065a6021405c48ed55cd5
mari_s <ms.mari.suzuki@gmail.com>
2020-11-06 21:18:16 +0900

    in progress 259. Separators Temp Files and File Stores added code to create temp file

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -33,5 +33,12 @@ public class Main {
         separator = FileSystems.getDefault().getSeparator();
         System.out.println(separator);
 
+        try {
+            Path tempFile = Files.createTempFile("myapp", ".appext");
+            System.out.println("Temporary file path = " + tempFile.toAbsolutePath());
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
