f48d5aef0a4c041cb6dff8a905219549cbb0a169
mari_s <ms.mari.suzuki@gmail.com>
2020-11-08 20:16:43 +0900

    in progress 262. Copy Entire Tree overrode visitFile method

--- src/com/timbuchalka/CopyFiles.java
+++ src/com/timbuchalka/CopyFiles.java
@@ -2,6 +2,7 @@ package com.timbuchalka;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -10,7 +11,7 @@ public class CopyFiles extends SimpleFileVisitor<Path> {
 
     private Path sourceRoot;
     private Path targetRoot;
-    
+
 
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
@@ -20,6 +21,35 @@ public class CopyFiles extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return super.preVisitDirectory(dir, attrs);
+
+        Path relativizedPath = sourceRoot.relativize(dir);
+        System.out.println("RelativizedPath = " + relativizedPath);
+        Path copyDir = targetRoot.resolve(relativizedPath);
+        System.out.println("Resolved path for copy = " + copyDir);
+
+        try {
+            Files.copy(dir, copyDir);
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            return FileVisitResult.SKIP_SUBTREE;
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path relativizedPath = sourceRoot.relativize(file);
+        System.out.println("RelativizedPath = " + relativizedPath);
+        Path copyDir = targetRoot.resolve(relativizedPath);
+        System.out.println("Resolved path for copy = " + copyDir);
+
+        try {
+            Files.copy(file, copyDir);
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return FileVisitResult.CONTINUE;
     }
 }
